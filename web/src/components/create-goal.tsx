d181e65070af2e6ea7bfed0c5d0756a793a667ac
Matheus Barbosa <122236890+mthszr@users.noreply.github.com>
2024-09-13 09:53:21 -0300

    update front

---
+++ web/src/components/create-goal.tsx
@@ -0,0 +1,107 @@
+import { X } from 'lucide-react'
+import {
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogTitle,
+} from './dialog'
+import { Label } from './label'
+import { Input } from './input'
+import { RadioGroup, RadioGroupIndicator, RadioGroupItem } from './radio-group'
+import { Button } from './button'
+import { Controller, useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const createGoalForm = z.object({
+  title: z.string().min(1, 'Informe a atividade que deseja realizar'),
+  desiredWeeklyFrequency: z.coerce.number().min(1).max(7),
+})
+
+export function CreateGoal() {
+  const { register, control } = useForm({
+    resolver: zodResolver(createGoalForm),
+  })
+
+  return (
+    <DialogContent>
+      <div className="flex flex-col gap-6 h-full">
+        <div className="flex flex-col gap-3">
+          <div className="flex items-center justify-between">
+            <DialogTitle>Cadastrar meta</DialogTitle>
+            <DialogClose>
+              <X className="size-5 text-zinc-600" />
+            </DialogClose>
+          </div>
+          <DialogDescription>
+            Adicione atividades que te fazem bem e que você quer continuar
+            praticando toda semana.
+          </DialogDescription>
+        </div>
+
+        <form action="" className="flex-1 flex flex-col justify-between">
+          <div className="flex flex-col gap-6">
+            <div className="flex flex-col gap-2">
+              <Label htmlFor="title">Qual a atividade?</Label>
+              <Input
+                id="title"
+                autoFocus
+                placeholder="Praticar exercícios, meditar, etc..."
+                {...register('title')}
+              />
+            </div>
+            <div className="flex flex-col gap-2">
+              <Label htmlFor="title">Quantas vezes na semana?</Label>
+              <Controller
+                control={control}
+                name="desiredWeeklyFrequency"
+                render={({ field }) => {
+                  return (
+                    <RadioGroup
+                      onValueChange={field.onChange}
+                      value={field.value}
+                    >
+                      <RadioGroupItem value="1">
+                        <RadioGroupIndicator />
+                        <span className="text-zinc-300 text-sm font-medium leading-none">
+                          1x na semana
+                        </span>
+                        <span className="text-lg leading-none">🥱</span>
+                      </RadioGroupItem>
+
+                      <RadioGroupItem value="2">
+                        <RadioGroupIndicator />
+                        <span className="text-zinc-300 text-sm font-medium leading-none">
+                          2x na semana
+                        </span>
+                        <span className="text-lg leading-none">🙂</span>
+                      </RadioGroupItem>
+
+                      <RadioGroupItem value="3">
+                        <RadioGroupIndicator />
+                        <span className="text-zinc-300 text-sm font-medium leading-none">
+                          3x na semana
+                        </span>
+                        <span className="text-lg leading-none">😎</span>
+                      </RadioGroupItem>
+                    </RadioGroup>
+                  )
+                }}
+              />
+            </div>
+          </div>
+
+          <div className="flex items-center gap-3">
+            <DialogClose asChild>
+              <Button type="button" className="flex-1" variant="secondary">
+                Fechar
+              </Button>
+            </DialogClose>
+
+            <Button className="flex-1">Salvar</Button>
+          </div>
+        </form>
+      </div>
+    </DialogContent>
+  )
+}
